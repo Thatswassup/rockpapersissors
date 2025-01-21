799a4f97382a284a886fd17d5e8039fed5cd58c9
Aaliyan <aaliyanqureshi6@gmail.com>
2025-01-20 20:08:41 -0500

    Add game round functionality and point system

--- rps.js
+++ rps.js
@@ -22,5 +22,80 @@ function getHumanChoice(){
     }
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound (humanChoice, computerChoice){
+    if ((humanChoice == "rock") && (computerChoice == "rock")){
+        console.log("It's a tie");
+    } 
+    else if ((humanChoice == "rock") && (computerChoice == "paper")){
+        computerScore++;
+        console.log("You Lose");
+        
+    }
+    else if ((humanChoice == "rock") && (computerChoice == "sissors")){
+        humanScore++;
+        console.log("You Win");
+
+    }
+    else if ((humanChoice == "paper") && (computerChoice == "rock")){
+        humanScore++;
+        console.log("You Win");
+
+    }
+    else if ((humanChoice == "paper") && (computerChoice == "paper")){
+        console.log("It's a tie");
+
+    }
+    else if ((humanChoice == "paper") && (computerChoice == "sissors")){
+        computerScore++;
+        console.log("You Lose");
+
+    }
+    else if ((humanChoice == "sissors") && (computerChoice == "rock")){
+        computerScore++;
+        console.log("You Lose");
+
+    }
+    else if ((humanChoice == "sissors") && (computerChoice == "paper")){
+        humanScore++;
+        console.log("You Win");
+
+    }
+    else if ((humanChoice == "sissors") && (computerChoice == "sissors")){
+        console.log("It's a tie");
+
+    }
+    else{
+
+        console.log("Invalid");
+    }
+}
+
+function playGame(){
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+}
+
+let humanScore = 0 ;
+let computerScore = 0 ;
+
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
+
+if (humanScore > computerScore){
+    console.log("You Win");
+}
+else if (humanScore < computerScore){
+    console.log("You Lose");
+}
+else{
+    console.log("Draw");
+}
+
+
+
+
